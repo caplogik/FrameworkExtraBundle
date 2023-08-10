e5ff4d8fdb614059d143e55bce059ff8af407c62
wryk <wryk@wryk.moe>
2023-08-10 22:05:37 +0200

    update discr union listener, add more doc

--- src/Form/EventSubscriber/DiscriminatedUnionSubscriber.php
+++ src/Form/EventSubscriber/DiscriminatedUnionSubscriber.php
@@ -28,32 +28,49 @@ final class DiscriminatedUnionSubscriber implements EventSubscriberInterface
         ];
     }
 
-    // This listener try to remap all root violations after form validation
-    // Without this all violations within PolymorphFormType are not correctly mapped
-    // Can be replaced by a dynamic violation mapping API like https://github.com/symfony/symfony/issues/40213
+    // Try to remap all root violations after form validation
+    // Without this all violations within DiscriminatedUnionType are not correctly mapped to their fields
+    // `error_bubbling` and `error_mapping` form configuration options are not enough for this use case
+    // Could be replaced by a dynamic violation mapping API like https://github.com/symfony/symfony/issues/40213
     public function remapRootViolations(FormEvent $event)
     {
         $form = $event->getForm();
 
-        if ($form->isRoot()) {
-            if (!$form instanceof ClearableErrorsInterface) {
-                throw new \RuntimeException('Root form needs to be error clearable to remap violations');
+        // This event listener need the whole form to works correctly
+        if (!$form->isRoot()) {
+            return;
+        }
+
+        // Remapped errors needs to removed form the root form and this is the only API allowing that
+        // This is not part of FormInterface but implemented in symfony by Form so :shrug:
+        // Weird but at least it *should* never throw
+        if (!$form instanceof ClearableErrorsInterface) {
+            throw new \RuntimeException(sprintf(
+                'Form needs to implements `%s` to be usable with `%s`',
+                ClearableErrorsInterface::class,
+                DiscriminatedUnionType::class
+            ));
+        }
+
+        // We clear all errors at the end, root errors not caused by discriminated unions are kept here to be added back to the form
+        $ignoredErrors = [];
+
+        foreach ($form->getErrors() as $error) {
+            $cause = $error->getCause();
+
+            // ConstraintViolation are required to have access to a propertyPath
+            if (!$cause instanceof ConstraintViolation) {
+                $ignoredErrors[] = $error;
+                continue;
             }
 
-            $ignoredErrors = [];
+            $propertyPath = $cause->getPropertyPath();
+            $properties = array_slice(explode('.', $propertyPath), 1);
+            $iteratedForm = $form;
 
-            foreach ($form->getErrors() as $error) {
-                $cause = $error->getCause();
-
-                if (!$cause instanceof ConstraintViolation) {
-                    $ignoredErrors[] = $error;
-                    continue;
-                }
-
-                $propertyPath = $cause->getPropertyPath();
-                $properties = array_slice(explode('.', $propertyPath), 1);
-                $iteratedForm = $form;
-
+            // Iterate the form tree using the properties of the violation
+            foreach ($properties as $property) {
+                // If the form is a DiscriminatedUnion the iterator is advanced to the correct inner form
                 if ($this->discriminatedUnionUtils->isDiscriminatedUnionType($iteratedForm)) {
                     $discriminator = $iteratedForm->get(DiscriminatedUnionType::FIELD_DISCRIMINATOR)->getData();
 
@@ -62,35 +79,23 @@ final class DiscriminatedUnionSubscriber implements EventSubscriberInterface
                         ->get($discriminator);
                 }
 
-
-                foreach ($properties as $property) {
-                    if (!$iteratedForm->has($property)) {
-                        $ignoredErrors[] = $error;
-                        break;
-                    }
-
-                    $iteratedForm = $iteratedForm->get($property);
-
-                    // If the form is a PolymorphFormType iteratedForm is updated to the valid sub form based on submitted data
-                    // if ($iteratedForm->getConfig()->getType()->getInnerType() instanceof PolymorphFormType) {
-                    //     $discriminator = $iteratedForm->get(PolymorphFormType::FIELD_DISCRIMINATOR)->getData();
-
-                    //     $iteratedForm = $iteratedForm
-                    //         ->get(PolymorphFormType::FIELD_INNER)
-                    //         ->get($discriminator);
-                    // }
+                // If the form tree cannot be iterated futher we ignore the error
+                if (!$iteratedForm->has($property)) {
+                    $ignoredErrors[] = $error;
+                    break;
                 }
 
-                $iteratedForm->addError($error);
+                $iteratedForm = $iteratedForm->get($property);
             }
+            $iteratedForm->addError($error);
+        }
 
-            // Remove all root errors, we don't have a more granular api available
-            $form->clearErrors();
+        // Remove all errors
+        $form->clearErrors();
 
-            // map again all ignored errors to root form
-            foreach ($ignoredErrors as $ignoredError) {
-                $form->addError($ignoredError);
-            }
+        // Add back all ignored errors
+        foreach ($ignoredErrors as $ignoredError) {
+            $form->addError($ignoredError);
         }
     }
 }
